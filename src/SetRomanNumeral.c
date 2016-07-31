c84ac909edbc4a628ce7019ae94f522b7f00a22f
anonymous <anonymous>
2016-07-31 14:05:11 -0400

    Fails for roman=3C

--- src/SetRomanNumeral.c
+++ src/SetRomanNumeral.c
@@ -4,16 +4,22 @@
 typedef enum { false, true } bool;
 
 bool SetRomanNumeral(struct RomanNumeral *romanNumeral, char *romanString) {
+  bool isRoman = false;
+
   if(strstr(romanString, "II") != NULL) {
     strcpy(romanNumeral->ones, "II");
+    isRoman = true;
   }
-  else {
+  else if(strstr(romanString, "I") != NULL) {
     strcpy(romanNumeral->ones, "I");
+    isRoman = true;
+  } else {
+    strcpy(romanNumeral->ones, "");
   }
 
   strcpy(romanNumeral->tens, "");
   strcpy(romanNumeral->hundreds, "");
   strcpy(romanNumeral->thousands, "");
 
-  return true;
+  return isRoman;
 }
