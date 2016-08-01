e9dc9402808ada561e172d1dff878402c6924176
anonymous <anonymous>
2016-07-31 22:06:43 -0400

    Refactored and fixed transient error.

--- src/SetRomanNumeral.c
+++ src/SetRomanNumeral.c
@@ -11,7 +11,7 @@ static void ClearRomanNumeral(struct RomanNumeral *romanNumeral) {
 }
 
 bool SetRomanNumeral(struct RomanNumeral *romanNumeral, char *romanString) {
-  bool isRoman = true;
+  bool isRoman = false;
   bool onesSymbolFound = false;
   bool tensSymbolFound = false;
   int i;
@@ -30,32 +30,28 @@ bool SetRomanNumeral(struct RomanNumeral *romanNumeral, char *romanString) {
 
   for(i=0; i<=tensNumberOfSymbols-1 && tensSymbolFound == false; ++i) {
     foundRomanPointer = strstr(shiftingRomanPointer, tensRomanSymbols[i]);
-    if(foundRomanPointer != NULL) {
+    if((foundRomanPointer != NULL) && (shiftingRomanPointer == foundRomanPointer)) {
       tensSymbolFound = true;
-      if(shiftingRomanPointer == foundRomanPointer) {
-        strcpy(romanNumeral->tens, tensRomanSymbols[i]);
-        shiftingRomanPointer = shiftingRomanPointer + tensSymbolSize[i];
-      }
-      else {
-        isRoman = false;
-      }
+      strcpy(romanNumeral->tens, tensRomanSymbols[i]);
+      shiftingRomanPointer += tensSymbolSize[i];
     }
   }
 
   for(i=0; i<=onesNumberOfSymbols-1 && onesSymbolFound == false; ++i) {
     foundRomanPointer = strstr(shiftingRomanPointer, onesRomanSymbols[i]);
-    if(foundRomanPointer != NULL) {
+    if((foundRomanPointer != NULL) && (shiftingRomanPointer == foundRomanPointer)) {
       onesSymbolFound = true;
-      if(shiftingRomanPointer == foundRomanPointer) {
-        strcpy(romanNumeral->ones, onesRomanSymbols[i]);
-      }
-      else {
-        isRoman = false;
-      }
+      strcpy(romanNumeral->ones, onesRomanSymbols[i]);
+      shiftingRomanPointer +=  onesSymbolSize[i];
     }
   }
-  if(onesSymbolFound == false && tensSymbolFound == false )
-    isRoman = false;
+
+  if(strlen(shiftingRomanPointer) == 0 ) {
+    isRoman = true;
+  }
+  else {
+    ClearRomanNumeral(romanNumeral);
+  }
 
   return isRoman;
 }
