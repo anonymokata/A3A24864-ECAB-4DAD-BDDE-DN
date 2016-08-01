3ee4d12cf83b26edcd25b80a0b5c9cda4c653bd1
anonymous <anonymous>
2016-07-31 20:40:57 -0400

    Set XI as valid.

--- src/SetRomanNumeral.c
+++ src/SetRomanNumeral.c
@@ -11,22 +11,51 @@ static void ClearRomanNumeral(struct RomanNumeral *romanNumeral) {
 }
 
 bool SetRomanNumeral(struct RomanNumeral *romanNumeral, char *romanString) {
-  bool isRoman = false;
-  bool symbolFound = false;
+  bool isRoman = true;
+  bool onesSymbolFound = false;
+  bool tensSymbolFound = false;
   int i;
+  char *shiftingRomanPointer = romanString;
+  char *foundRomanPointer;
 
-  char numberOfSymbols = 9;
-  char *romanSymbols[] = {"IX", "VIII", "VII", "VI", "IV", "V", "III", "II", "I"};
+  char onesNumberOfSymbols = 9;
+  char onesSymbolSize[] = {2, 4, 3, 2, 2, 1, 3, 2, 1};
+  char *onesRomanSymbols[] = {"IX", "VIII", "VII", "VI", "IV", "V", "III", "II", "I"};
+
+  char tensNumberOfSymbols = 1;
+  char tensSymbolSize[] = {1};
+  char *tensRomanSymbols[] = {"X"};
 
   ClearRomanNumeral(romanNumeral);
 
-  for(i=0; i<=numberOfSymbols-1 && symbolFound == false; ++i) {
-    if(strstr(romanString, romanSymbols[i]) != NULL) {
-      strcpy(romanNumeral->ones, romanSymbols[i]);
-      isRoman = true;
-      symbolFound = true;
+  for(i=0; i<=tensNumberOfSymbols-1 && tensSymbolFound == false; ++i) {
+    foundRomanPointer = strstr(shiftingRomanPointer, tensRomanSymbols[i]);
+    if(foundRomanPointer != NULL) {
+      tensSymbolFound = true;
+      if(shiftingRomanPointer == foundRomanPointer) {
+        strcpy(romanNumeral->tens, tensRomanSymbols[i]);
+        shiftingRomanPointer = shiftingRomanPointer + tensSymbolSize[i];
+      }
+      else {
+        isRoman = false;
+      }
     }
   }
+
+  for(i=0; i<=onesNumberOfSymbols-1 && onesSymbolFound == false; ++i) {
+    foundRomanPointer = strstr(shiftingRomanPointer, onesRomanSymbols[i]);
+    if(foundRomanPointer != NULL) {
+      onesSymbolFound = true;
+      if(shiftingRomanPointer == foundRomanPointer) {
+        strcpy(romanNumeral->ones, onesRomanSymbols[i]);
+      }
+      else {
+        isRoman = false;
+      }
+    }
+  }
+  if(onesSymbolFound == false && tensSymbolFound == false )
+    isRoman = false;
 
   return isRoman;
 }
