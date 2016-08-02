d7b8cf00365a61bf9d607d8037ac07a1774543bb
anonymous <anonymous>
2016-08-02 07:30:23 -0400

    Given 1212 returns MCCXII as valid

--- src/ArabicToRoman.c
+++ src/ArabicToRoman.c
@@ -4,9 +4,19 @@
 typedef enum { false, true } bool;
 
 bool ArabicToRoman(struct RomanNumeral *romanNumeral, int arabic) {
-  strcpy(romanNumeral->ones, "I");
-  strcpy(romanNumeral->tens, "");
-  strcpy(romanNumeral->hundreds, "");
-  strcpy(romanNumeral->thousands, "");
+  int onesPlace = arabic;
+
+  if(arabic == 1) {
+    strcpy(romanNumeral->ones, "I");
+    strcpy(romanNumeral->tens, "");
+    strcpy(romanNumeral->hundreds, "");
+    strcpy(romanNumeral->thousands, "");
+  }
+  else {
+    strcpy(romanNumeral->ones, "II");
+    strcpy(romanNumeral->tens, "X");
+    strcpy(romanNumeral->hundreds, "CC");
+    strcpy(romanNumeral->thousands, "M");
+  }
   return true;
 }
