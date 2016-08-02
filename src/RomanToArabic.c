8537cb10d96c31ab54fbc63e854acc6a63e01950
anonymous <anonymous>
2016-08-01 21:31:05 -0400

    Given MMCVXCIX returns 2999

--- src/RomanToArabic.c
+++ src/RomanToArabic.c
@@ -2,12 +2,12 @@
 #include "RomanNumeral.h"
 
 #define ROW 4
-#define COL 9
+#define COL 10
 
 char *romanSymbols[ROW][COL] = {
-  {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII" },
-  {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX" },
-  {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC" },
+  {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" },
+  {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" },
+  {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" },
   {"", "M", "MM", "MMM" }
 };
 
