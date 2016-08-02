00778b97bbb1a1d73a6fc0055c5f206e3ab167c3
anonymous <anonymous>
2016-08-02 17:51:00 -0400

    Given 707 returns DCCVII as valid

--- src/ArabicToRoman.c
+++ src/ArabicToRoman.c
@@ -4,13 +4,13 @@
 typedef enum { false, true } bool;
 
 #define ROW 4
-#define COL 7
+#define COL 8
 
 char *romanSymbols[ROW][COL] = {
-  {"", "I", "II", "III", "IV", "V", "VI" },
-  {"", "X", "XX", "XXX", "XL", "L", "LX" },
-  {"", "C", "CC", "CCC", "CD", "D", "DC" },
-  {"", "M", "MM", "MMM", "", "" }
+  {"", "I", "II", "III", "IV", "V", "VI", "VII" },
+  {"", "X", "XX", "XXX", "XL", "L", "LX", "" },
+  {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC" },
+  {"", "M", "MM", "MMM", "", "", "" }
 };
 
 bool ArabicToRoman(struct RomanNumeral *romanNumeral, int arabic) {
