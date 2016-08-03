cecb42bc3af2c0e48b9384eb682c95f1f18a3e51
anonymous <anonymous>
2016-08-02 22:12:03 -0400

    Given buffer of 5 returns invalid.

--- src/GetRomanNumeralString.c
+++ src/GetRomanNumeralString.c
@@ -1,11 +1,15 @@
 #include <string.h>
 #include "RomanNumeral.h"
+#define MIN_ROMAN_NUMERAL_STRING_BUFFER_SIZE 21
 
 typedef enum { false, true } bool;
 
 bool GetRomanNumeralString(struct RomanNumeral *romanNumeral, char *romanNumeralString,
   int romanNumeralStringBufferSize) {
 
+  if(romanNumeralStringBufferSize < MIN_ROMAN_NUMERAL_STRING_BUFFER_SIZE) {
+    return false;
+  }
   strcpy(romanNumeralString, romanNumeral->thousands);
   strcat(romanNumeralString, romanNumeral->hundreds);
   strcat(romanNumeralString, romanNumeral->tens);
